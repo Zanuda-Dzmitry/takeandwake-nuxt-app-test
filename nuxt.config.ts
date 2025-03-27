e5b05425abe6c850118a0aa71f4146f49cf06149
Dzmitry_Shaviaka <krayzerkyn@gmail.com>
2025-03-27 15:15:29 +0300

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/ui", "@pinia/nuxt"],
+  app: {
+    baseURL: "/takeandwake-nuxt-app-test/",
+  },
+
+  nitro: {
+    output: {
+      dir: "./dist/takeandwake-nuxt-app-test",
+    },
+  },
 });
